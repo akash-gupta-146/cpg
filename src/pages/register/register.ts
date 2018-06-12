e51afc4346611cbcb653d274d2f8784ec6b9d6e7
Nitin Negi <nitinnegi0810@gmail.com>
2018-06-12 18:09:58 +0530

    address fields added in register form

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
+
+// const enum AddressType{
+//   Home = 'Home', Office = 'Office', Permanent = 'Permanent'
+// };
 
 
 @IonicPage()
@@ -9,9 +13,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
-
+  
   registerForm: FormGroup;
   submitAttempt = false;
+  readonly addressTypes: Array<any> = ['Home', 'Office', 'Permanent'];
 
   constructor(
     public navCtrl: NavController,
@@ -27,21 +32,46 @@ export class RegisterPage {
     this.registerForm = this.fb.group({
       name: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z ]*')])],
       contact: ['', Validators.compose([Validators.required, Validators.pattern('^[0-9]+$')])],
-      email: ['', Validators.compose([Validators.required, Validators.email])],
+      email: ['', Validators.compose([Validators.email])],
       password: ['', Validators.compose([Validators.required])],
       confirmPassword: ['', Validators.compose([Validators.required])],
-      address: [''],
+      addresses:new FormArray([])
     });
+  }
+
+  
+  addAddress(){
+    (<FormArray>this.registerForm.controls.addresses).push(this.giveAddressInstance());
+  }
+
+  removeAddress(index:number){
+    (<FormArray>this.registerForm.controls.addresses).removeAt(index);
+  }
+
+  giveAddressInstance() {
+
+    return new FormGroup({
+      type: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+      state: new FormControl('', Validators.required),
+      country: new FormControl('', Validators.required),
+      postalCode: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+      phone: new FormControl('')
+    });
+  }
+
+  isAddressTypeAlreadySelected(type: string) {
+    return this.registerForm.controls.addresses.value.findIndex((control: any) => control.type == type) > -1;
   }
 
   // getter properties for easy access of controls in html
   get name() { return this.registerForm.get('name'); }
-  get username() { return this.registerForm.get('username'); }
   get contact() { return this.registerForm.get('contact'); }
   get email() { return this.registerForm.get('email'); }
   get password() { return this.registerForm.get('password'); }
   get confirmPassword() { return this.registerForm.get('confirmPassword'); }
-  get address() { return this.registerForm.get('address'); }
+  get addresses() { return this.registerForm.get('addresses') as FormArray; }
 
 
   isSameAsPassword() {
@@ -56,7 +86,7 @@ export class RegisterPage {
     this.submitAttempt = true;
     if (this.registerForm.invalid) { return; }
   }
-    
+
   dismiss() {
     this.viewCtrl.dismiss();
   }
--- src/providers/custom-http.service.ts
+++ src/providers/custom-http.service.ts
@@ -5,10 +5,10 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import { BASEURL } from './app.constants';
-declare var URLPREFIX;
-declare var ROLE;
-declare const SockJS;
-declare const Stomp;
+// declare var URLPREFIX;
+// declare var ROLE;
+// declare const SockJS;
+// declare const Stomp;
 
 @Injectable()
 export class CustomHttpService {
@@ -17,7 +17,7 @@ export class CustomHttpService {
 
     private getAccessToken() {
 
-        let basicToken = "nxtlifefeedbacksystem:kabuliwala";
+        let basicToken = "nxtlifecustomersupport:suvidha";
 
         return !localStorage.getItem('access_token') ? 'Basic ' + btoa(basicToken) : 'Bearer ' + localStorage.getItem('access_token');
     }
@@ -26,7 +26,7 @@ export class CustomHttpService {
 
         let requestHeaders = new HttpHeaders()
             .set('Authorization', this.getAccessToken())
-            .set('Content-Type', 'application/json');
+            .set('X-Content', 'customer');
         if (optionalHeaders) {
             for (const header of optionalHeaders.keys()) {
                 requestHeaders = requestHeaders.append(header, optionalHeaders.get(header));
@@ -37,38 +37,23 @@ export class CustomHttpService {
 
 
 
-    // get(url: string, options?: HttpHeaders) {
+    get(url: string, options?: HttpHeaders) {
 
-    //     const headers = this.addHeaders(options);
-    //     let _url: string;
-    //     if (ROLE) {
-    //         // in case of admin.mngmnt, each request contains ROLE if ROLE exists
-    //         _url = BASEURL + (URLPREFIX ? '/' + URLPREFIX : '') + '/' + ROLE + url;
-    //     } else {
-    //         _url = BASEURL + (URLPREFIX ? '/' + URLPREFIX : '') + url;
+        const headers = this.addHeaders(options);
 
-    //     }
+        return this.httpClient.get(BASEURL+url, { headers: headers, observe: 'response' })
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
 
-    //     return this.httpClient.get(_url, { headers: headers, observe: 'response' })
-    //         .map(this.extractData)
-    //         .catch(this.handleError);
-    // }
+    post(url: string, body: any, options?: HttpHeaders) {
 
-    // post(url: string, body: any, options?: HttpHeaders) {
-
-    //     let headers = this.addHeaders(options);
-    //     let _url: string;
-    //     if (ROLE) {
-    //         // in case of admin.mngmnt, each request contains ROLE if ROLE exists
-    //         _url = BASEURL + (URLPREFIX ? '/' + URLPREFIX : '') + '/' + ROLE + url;
-    //     } else {
-    //         _url = BASEURL + (URLPREFIX ? '/' + URLPREFIX : '') + url;
-
-    //     }
-    //     return this.httpClient.post(_url, body, { headers: headers, observe: 'response' })
-    //         .map(this.extractData)
-    //         .catch(this.handleError);
-    // }
+        const headers = this.addHeaders(options);
+    
+        return this.httpClient.post(BASEURL+url, body, { headers: headers, observe: 'response' })
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
 
     // put(url: string, body: any, options?: HttpHeaders) {
 
@@ -100,31 +85,31 @@ export class CustomHttpService {
     //         .catch(this.handleError);
     // }
 
-    // private extractData(res: HttpResponse<any>) {
+    private extractData(res: HttpResponse<any>) {
 
-    //     // console.log('inside extract data', res);
-    //     return res.body || res.status;
-    // }
+        // console.log('inside extract data', res);
+        return res.body || res.status;
+    }
 
-    // private handleError(err: HttpErrorResponse) {
-    //     // console.log('inside handle error', err);
-    //     let errorInfo: any = {};
+    private handleError(err: HttpErrorResponse) {
+        // console.log('inside handle error', err);
+        let errorInfo: any = {};
 
-    //     if (err.error instanceof Error || err.error instanceof ProgressEvent) {
-    //         /**A client-side or network error occurred. Handle it accordingly.*/
-    //         // console.log('An error occurred:', );
-    //         errorInfo.status = err.status;
-    //         errorInfo.status == 0 ? errorInfo.msg = "No Internet, Check Your connection Or Try again" : errorInfo.msg = err.message || 'Some Error Occured';
-    //     }
-    //     else {
-    //         /**The backend returned an unsuccessful response code.*/
-    //         // console.log('Server occurred:', err);
-    //         errorInfo.status = err.status;
-    //         errorInfo.msg = err.error.message || err.error.error || 'Internal Server Error';
-    //     }
-    //     return Observable.throw(errorInfo);
+        if (err.error instanceof Error || err.error instanceof ProgressEvent) {
+            /**A client-side or network error occurred. Handle it accordingly.*/
+            // console.log('An error occurred:', );
+            errorInfo.status = err.status;
+            errorInfo.status == 0 ? errorInfo.msg = "No Internet, Check Your connection Or Try again" : errorInfo.msg = err.message || 'Some Error Occured';
+        }
+        else {
+            /**The backend returned an unsuccessful response code.*/
+            // console.log('Server occurred:', err);
+            errorInfo.status = err.status;
+            errorInfo.msg = err.error.message || err.error.error || 'Internal Server Error';
+        }
+        return Observable.throw(errorInfo);
 
-    // }
+    }
 
     // getSockJs() {
 
