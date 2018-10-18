20adee57ff0272ff267d21e9ceda1896fb3d59a6
Nitin Negi <nitinnegi0810@gmail.com>
2018-10-18 15:17:19 +0530

    rating shown in main incidents page

--- src/pages/incidents/incidents.module.ts
+++ src/pages/incidents/incidents.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { IncidentsPage } from './incidents';
+import { Ionic2RatingModule } from 'ionic2-rating';
 
 @NgModule({
   declarations: [
     IncidentsPage,
   ],
   imports: [
+    Ionic2RatingModule,
     IonicPageModule.forChild(IncidentsPage),
   ],
 })
